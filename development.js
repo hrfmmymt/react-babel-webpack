fcf07a44dfdb858158c89012bf46177eb4c196e3
hrfmmymt <hrfmmymt@gmail.com>
2016-12-19 18:58:42 +0900

    modified.

--- development.js
+++ development.js
@@ -1,23 +1,65 @@
 import path from "path";
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
+require("babel-polyfill");
+
+const webpack = require("webpack");
 
 const src  = path.resolve(__dirname, "src");
 const dist = path.resolve(__dirname, "dist");
+const DEBUG = !process.argv.includes("--release");
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "../index.html"
+  }),
+  new webpack.optimize.OccurenceOrderPlugin()
+];
+
+if(!DEBUG){
+  plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({ compress: { screw_ie8: true, warnings: false } }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  );
+}
 
 export default {
-  entry: src + "/index.jsx",
+  entry: src + "/js/index.jsx",
 
   output: {
     path: dist,
-    filename: "bundle.js"
+    filename: "js/bundle.js"
   },
+
+  devServer: {
+    contentBase: "dist"
+  },
+
+  devtool: DEBUG ? "cheap-module-eval-source-map" : false,
 
   module: {
     loaders: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        loader: "babel"
+        loaders: [
+          "react-hot",
+          "babel"
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style",
+          "css?modules",
+          "postcss"
+        ]
       }
     ]
   },
@@ -26,10 +68,11 @@ export default {
     extensions: ["", ".js"]
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html'
-    })
+  plugins: plugins,
+
+  postcss: [
+    require("autoprefixer")(),
+    require("postcss-custom-properties")(),
+    require("postcss-nested")()
   ]
 }
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -5,6 +5,8 @@ import Hello from "./components/hello.jsx";
 import ReactBabel from "./components/react-babel.jsx";
 import App from "./components/app.jsx";
 
+import styles from "../css/style.css";
+
 render(
   <div>
     <Hello />
